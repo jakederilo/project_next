3932076ae28e60171dc372bd5609e67cea618880
jake derilo <jakederilo@thelewiscollege.edu.ph>
2024-02-10 14:07:57 +0800

    update

--- pages/post.jsx
+++ pages/post.jsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { BsPersonFill, BsThreeDotsVertical } from 'react-icons/bs';
 
 const Post = () => {
-  const [users, setUsers] = useState([]);
+  const [post, setPosts] = useState([]);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts/')
       .then(response => response.json())
-      .then(usersData => {
-        setUsers(usersData);
+      .then(postData => {
+        setUsers(postData);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
@@ -30,17 +30,16 @@ const Post = () => {
             <span className='hidden sm:grid'>City</span>
           </div>
           <ul>
-            {users.map((user, userid) => (
-              <li key={userid} className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
+            {post.map((post, index) => (
+              <li key={index} className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
                 <div className='flex items-center'>
                   <div className='bg-purple-100 p-3 rounded-lg'>
                     <BsPersonFill className='text-purple-800' />
                   </div>
-                  <p className='pl-4'>{user.name}</p>
+                  <p className='pl-4'>{post.title}</p>
                 </div>
-                <p className='text-gray-600 sm:text-left text-right'>{user.email}</p>
-                <p className='hidden md:flex'>{user.address.street}</p>
-                <p className='hidden sm:flex'>{user.address.city}</p>
+                <p className='text-gray-600 sm:text-left text-right'>{post.email}</p>
+                <p className='hidden md:flex'>{user.body}</p>
               </li>
             ))}
           </ul>
